6e64486229a2657f8218e36ceefc971e9221308a
Tavares <gabrielgomestavares2001@gmail.com>
2023-03-09 03:43:18 -0300

    verificação de hora e dia marcados

--- criacaodb.php
+++ criacaodb.php
@@ -11,17 +11,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $hora = $hora_inicio;
 
+    
     for ($i = $hora_inicio; $i < $hora_inicio + $duracao; $i++) {
-        
-        $sql = "INSERT INTO wr1 (dia, hora, nome) VALUES ('$dia', '$hora', '$nome')";
-        if (mysqli_query($conn, $sql)) {
-            echo "Nome adicionado com sucesso para $dia às $hora:00!<br>";
-        } else {
-            echo "Erro ao adicionar nome: " . mysqli_error($conn);
+
+        $validate_consulta = "SELECT * FROM wr1 WHERE dia LIKE $dia AND hora LIKE $hora";
+        $validade = mysqli_query($conn, $validate_consulta);
+
+        if (mysqli_num_rows($validade) == 0){
+            header('Location: index.php');
+            return;
         }
+    }
+
+    $hora = $hora_inicio;
+
+
+    for ($i = $hora_inicio; $i < $hora_inicio + $duracao; $i++) {
+
+        $sql = "INSERT INTO wr1 (dia, hora, nome) VALUES ('$dia', '$hora', '$nome')";
+            if (mysqli_query($conn, $sql)) {
+                echo "Nome adicionado com sucesso para $dia às $hora:00!<br>";
+            } else {
+                echo "Erro ao adicionar nome: " . mysqli_error($conn);
+            }
 
         $hora ++;
-
+        
     }
 }
 
